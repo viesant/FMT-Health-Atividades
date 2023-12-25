bc7e2be498ee7039b842ba2334e35bd5d4624d43
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2023-12-25 20:01:27 +0100

    end tasks
    added js code

---
+++ Atividades/M1S01/script.js
@@ -0,0 +1,9 @@
+function changeColor(color){
+    // document.getElementById('demo').style.fontSize='35px';
+    // document.getElementsByTagName('header').style.fontSize='50px';
+    document.getElementById('header').style.backgroundColor=color;
+}
+
+function showTitle(title){
+    window.alert(title);
+}
