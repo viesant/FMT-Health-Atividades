cfffa05a168dfa4dfb90284ce5d91ac0ff021342
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-01-05 21:32:15 +0100

    correção event listener

--- Atividades/M1S01/script.js
+++ Atividades/M1S01/script.js
@@ -1,9 +1,18 @@
-function changeColor(color){
-    // document.getElementById('demo').style.fontSize='35px';
-    // document.getElementsByTagName('header').style.fontSize='50px';
-    document.getElementById('header').style.backgroundColor=color;
-}
+// listener mudar cor cabeçalho
+let header = document.getElementById('iHeader');
+header.addEventListener("mouseenter", () =>{
+    console.log("mouse entrou");
+    header.style.backgroundColor = 'rgb(48, 43, 43)';
+})
+header.addEventListener("mouseleave", () =>{
+    console.log("mouse saiu");
+    header.style.backgroundColor = 'black';
+})
 
-function showTitle(title){
-    window.alert(title);
-}
+// listener mostrar título quando clica no artigo
+let articles = document.querySelectorAll('article');
+articles.forEach(article => {
+    article.addEventListener('click', e => {
+        alert(e.currentTarget.querySelector('h2').innerText)
+    })
+})
