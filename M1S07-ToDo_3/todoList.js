5efb990136b3d0d28284bc79ac48d3559cd8db17
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-02-29 01:08:46 +0100

    [M1S07] Ex. 3

--- M1S07-ToDo_3/todoList.js
+++ M1S07-ToDo_3/todoList.js
@@ -13,10 +13,33 @@ function addNewTask() {
   }
   // incrementa id da nova task
   taskId++;
-  let newTask = newTaskField.value;
 
+  // cria novo elemento list
   let newList = document.createElement("li");
-  newList.innerHTML = `<input type="checkbox" name="" id="task${taskId}" /><label for="task${taskId}">${newTask}</label><button class="delete" type="button"><i class="fa-solid fa-trash"></i></button>`;
+
+  // cria html do item com checkbox, nome tarefa e botão excluir
+  newList.innerHTML = `<input type="checkbox" name="" id="task${taskId}" /><label for="task${taskId}">${newTaskField.value}</label><button class="delete" type="button"><i class="fa-solid fa-trash"></i></button>`;
+
+  // inclui elemento no ul
   taskList.appendChild(newList);
-  newTaskField.value = 0;
+
+  // apaga valor do campo nova tarefa
+  newTaskField.value = "";
+
+  // busca botao exlcuir no novo item
+  const deleteButton = newList.querySelector(".delete");
+
+  // cria eventlistener pro novo novo botão excluir
+  deleteButton.addEventListener("click", deleteTask);
+}
+
+function deleteTask(event) {
+  // abre confirmação para deletar
+  let doDelete = confirm("Deseja apagar a tarefa da lista?");
+  if (doDelete) {
+    // busca item da lista
+    const taskToDelete = event.target.closest("li");
+    // apaga item
+    taskToDelete.remove();
+  }
 }
