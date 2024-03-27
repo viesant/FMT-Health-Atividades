c046e38984ba429178c932b2ab3dad5046a86c44
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-03-27 14:52:01 +0100

    [M1S10] Ex 02

--- M1S10-KeepHealth2/src/app/app.routes.ts
+++ M1S10-KeepHealth2/src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DietComponent } from './diet/diet.component';
+import { DietDetailComponent } from './diet/diet-detail/diet-detail.component';
 
 export const routes: Routes = [
   {
@@ -19,5 +22,22 @@ export const routes: Routes = [
   {
     path: 'cadastro',
     component: CadastroComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'diet',
+    children: [
+      {
+        path: '',
+        component: DietComponent,
+      },
+      {
+        path: ':id',
+        component: DietDetailComponent,
+      },
+    ],
   },
 ];
