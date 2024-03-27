4d4183ec4e7dac91872f237ea758b5cbb6b747eb
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-03-27 14:20:39 +0100

    [M1S10] Ex 01 - Projeto Saúde Ativa

--- M1S10-KeepHealth2/src/app/app.component.ts
+++ M1S10-KeepHealth2/src/app/app.component.ts
@@ -1,13 +1,112 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'M1S09-KeepHealth';
+  title = 'KeepHealth';
+  alimentos = [
+    {
+      id: 1,
+      name: 'Abacate',
+      description:
+        'O abacate é uma fruta versátil, rica em gorduras saudáveis e nutrientes essenciais.',
+      qttCalories: 160, // Valor fictício, ajuste conforme necessário
+      qttDaysFeed: 3,
+      imageLink: 'link_para_imagem',
+    },
+    {
+      id: 2,
+      name: 'Banana',
+      description:
+        'A banana é uma fruta rica em potássio, que ajuda na saúde dos músculos e do coração.',
+      qttCalories: 105, // Valor fictício, ajuste conforme necessário
+      qttDaysFeed: 2,
+      imageLink: 'link_para_imagem',
+    },
+    {
+      id: 3,
+      name: 'Maçã',
+      description:
+        'A maçã é uma fruta muito popular, rica em fibras e antioxidantes.',
+      qttCalories: 95, // Valor fictício, ajuste conforme necessário
+      qttDaysFeed: 1,
+      imageLink: 'link_para_imagem',
+    },
+    {
+      id: 4,
+      name: 'Cenoura',
+      description:
+        'A cenoura é um vegetal rico em betacaroteno, importante para a saúde da visão.',
+      qttCalories: 25, // Valor fictício, ajuste conforme necessário
+      qttDaysFeed: 2,
+      imageLink: 'link_para_imagem',
+    },
+    {
+      id: 5,
+      name: 'Ovo',
+      description:
+        'O ovo é uma excelente fonte de proteína e contém muitos nutrientes essenciais.',
+      qttCalories: 70, // Valor fictício, ajuste conforme necessário
+      qttDaysFeed: 4,
+      imageLink: 'link_para_imagem',
+    },
+    {
+      id: 6,
+      name: 'Salmão',
+      description:
+        'O salmão é um peixe rico em ácidos graxos ômega-3, benéficos para a saúde cardiovascular.',
+      qttCalories: 220, // Valor fictício, ajuste conforme necessário
+      qttDaysFeed: 2,
+      imageLink: 'link_para_imagem',
+    },
+    {
+      id: 7,
+      name: 'Brócolis',
+      description:
+        'O brócolis é um vegetal rico em vitaminas e minerais, além de ser uma boa fonte de fibras.',
+      qttCalories: 50, // Valor fictício, ajuste conforme necessário
+      qttDaysFeed: 2,
+      imageLink: 'link_para_imagem',
+    },
+    {
+      id: 8,
+      name: 'Quinoa',
+      description:
+        'A quinoa é um grão rico em proteínas e fibras, sendo uma excelente opção para uma dieta equilibrada.',
+      qttCalories: 120, // Valor fictício, ajuste conforme necessário
+      qttDaysFeed: 3,
+      imageLink: 'link_para_imagem',
+    },
+    {
+      id: 9,
+      name: 'Iogurte',
+      description:
+        'O iogurte é uma fonte de cálcio e probióticos, benéficos para a saúde intestinal.',
+      qttCalories: 150, // Valor fictício, ajuste conforme necessário
+      qttDaysFeed: 2,
+      imageLink: 'link_para_imagem',
+    },
+    {
+      id: 10,
+      name: 'Nozes',
+      description:
+        'As nozes são oleaginosas ricas em ácidos graxos insaturados e antioxidantes.',
+      qttCalories: 185, // Valor fictício, ajuste conforme necessário
+      qttDaysFeed: 1,
+      imageLink: 'link_para_imagem',
+    },
+  ];
+  constructor() {}
+  ngOnInit(): void {}
+
+  gravarDados() {
+    localStorage.setItem('alimentos', JSON.stringify(this.alimentos));
+  }
 }
--- M1S10-KeepHealth2/src/app/app.routes.ts
+++ M1S10-KeepHealth2/src/app/app.routes.ts
@@ -5,15 +5,19 @@ import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
     path: 'home',
     component: HomeComponent,
   },
   {
     path: 'cadastro',
     component: CadastroComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
   },
 ];
